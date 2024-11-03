1837ab07c80e3defbdf985e69cd977636dbc3f8d
ebu13 <ebubekir13042003@gmail.com>
2024-11-03 11:43:02 +0300

    last

--- src/librarymenagmentsystem/IUser.java
+++ src/librarymenagmentsystem/IUser.java
@@ -1,7 +1,9 @@
 public interface IUser {
+
     String getUserId();
     String getName();
     String getEmail();
     boolean borrow(ILibraryItem item);
     boolean returnItem(ILibraryItem item);
+    
 }
